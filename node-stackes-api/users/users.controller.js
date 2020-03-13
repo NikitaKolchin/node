6f40f6ce45305c3d690b2ab346da42db4a86a080
Колчин Никита Вячеславович <kolchin.nv@gmail.com>
2020-03-13 17:14:27 +0300

    1303

--- node-stackes-api/users/users.controller.js
+++ node-stackes-api/users/users.controller.js
@@ -24,18 +24,25 @@ router.get('/', async (req, res, next) => {
 
 //add new user
 router.post('/', async (req, res, next) => {
+  if (req.user.isAdmin){
     const user = new User({
-        username: req.body.username, 
-        password: req.body.password, 
-        firstName: req.body.firstName, 
-        lastName: req.body.lastName
-      });
-      try {
-        const newUser = await user.save();
-        res.status(201).json({ newUser });
-      } catch (err) {
-        res.status(400).json({ message: err.message });
-      }
+      username: req.body.username, 
+      password: req.body.password, 
+      firstName: req.body.firstName, 
+      lastName: req.body.lastName,
+      isAdmin: req.body.isAdmin
+    });
+    try {
+      const newUser = await user.save();
+      res.status(201).json({ newUser });
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  }
+  else {
+    res.status(403).json({ message: "Need admin" });
+  }    
+
 });
 
 //middleware
@@ -60,6 +67,7 @@ router.get('/:id',getUser, (req, res) => {
 
 //delete One user
 router.delete("/:id", getUser, async (req, res) => {
+  if (req.user.isAdmin){
     try {
       const deletedUserName = res.user.username; 
       await res.user.deleteOne();
@@ -67,29 +75,45 @@ router.delete("/:id", getUser, async (req, res) => {
     } catch (err) {
       res.status(500).json({ message: err.message });
     }
+  }
+  else {
+    res.status(403).json({ message: "Need admin" });
+  }  
 });
 
 //delete all users
 router.delete("/", async (req, res) => {
-  try {
-    await User.deleteMany();
-    res.json({ message: "All users has been deleted" });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+  if (req.user.isAdmin){
+    try {
+      await User.deleteMany();
+      res.json({ message: "All users has been deleted" });
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
   }
+  else {
+    res.status(403).json({ message: "Administrator rights required" });
+  } 
 });
 
 //update one user
 router.put("/:id", getUser, async (req, res) => {
+  if (req.user.isAdmin){
     try {
         const updatedUser = {username: req.body.username, 
                         password: req.body.password,
                         firstName: req.body.firstName, 
-                        lastName: req.body.lastName };  
+                        lastName: req.body.lastName,
+                        isAdmin: req.body.isAdmin
+                       };  
         const userArterUpdate = await User.findOneAndUpdate({_id: res.user.id}, updatedUser, {new: true});
         res.json(userArterUpdate);
     } catch (err) {
         res.status(400).json({ message: err.message });
     }
+  }
+  else {
+    res.status(403).json({ message: "Administrator rights required" });
+  }  
 });
 
