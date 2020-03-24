e0472b8880503775025b5e49c62a6880f31e38cc
Nikita Kolchin <kolchin.nv@gmail.com>
2020-03-24 17:25:13 +0300

    2303

--- mongo.js
+++ mongo.js
@@ -7,7 +7,7 @@ mongoClient.connect(function(err, client){
       
     const db = client.db("test");
     const collection = db.collection("users");
-    let user = {name: "Tom", age: 23};
+    let user = {name: "Tom1", age: 23};
     collection.find().toArray(function(err, results){
                  
         console.log(results);
