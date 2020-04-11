1e7fd7ba4db70b4d186c4fc6f40c4145621d581f
Nikita Kolchin <kolchin.nv@gmail.com>
2020-04-11 17:14:28 +0300

    1004

--- react-client/src/AdminPage.jsx
+++ react-client/src/AdminPage.jsx
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useEffect } from 'react';
 
-class AdminPage extends React.Component{
-    constructor(props) {
-        super(props);
+// class AdminPage extends React.Component{
+//     constructor(props) {
+//         super(props);
   
-        this.state = {
-            user: {}
-        };
-    } 
+//         this.state = {
+//             user: {}
+//         };
+//     } 
   
-    componentDidMount() {
-        this.setState({ 
-            user: JSON.parse(localStorage.getItem('user')),
-        });
-    }
+//     componentDidMount() {
+//         this.setState({ 
+//             user: JSON.parse(localStorage.getItem('user')),
+//         });
+//     }
   
-    render(){  
-        const { user } = this.state;
-        return user.isAdmin? <h2>Привет Админ {user.username}</h2>:<h2>Привет юзер {user.username}</h2>;
-    }
+//     render(){  
+//         const { user } = this.state;
+//         return user.isAdmin? <h2>Привет Админ {user.username}</h2>:<h2>Привет юзер {user.username}</h2>;
+//     }
+// }
+
+function AdminPage(){
+    let [user, setUser] = useState([]);
+    useEffect(()=> setUser(JSON.parse(localStorage.getItem('user'))),[]);
+    return user.isAdmin? <h2>Привет Админ {user.username}</h2>:<h2>Привет юзер {user.username}</h2>;
 }
-  
-export  {AdminPage};
+
+export {AdminPage};
