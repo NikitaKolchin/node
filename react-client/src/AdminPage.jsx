6d1b04d770234ce5677fc3bf38837f734974a27a
Nikita Kolchin <kolchin.nv@gmail.com>
2020-04-28 19:46:56 +0300

    2804

--- react-client/src/AdminPage.jsx
+++ react-client/src/AdminPage.jsx
@@ -1,42 +1,49 @@
 import React, {useState, useEffect} from 'react';
-import {User} from './User'
 import { userService } from './user.service';
+import TextField from '@material-ui/core/TextField';
+import { Alert, AlertTitle} from '@material-ui/lab'
+import { Checkbox, Button, Grid, Typography, } from '@material-ui/core';
 
 
 function AdminPage(){
     let [currentUser, setCurrentUser] = useState({});
     let [newUser, setNewUser] =useState('');
     let [users, setUsers] = useState({loading: true});
+    let [answer, setAnswer] = useState(false);
 
     useEffect(async ()=>{
         setCurrentUser(JSON.parse(localStorage.getItem('user')));
         setUsers(await userService.getAll())      
     },[]);
 
-    function handleAddItem(index) {
-       userService.addOneUser({'username': newUser.username}).then(newUser => setUsers(users => users.concat(newUser)));
+    function handleAddItem() {
+       userService.addOneUser({'username': newUser.username}).then(
+           newUser => {
+               setUsers(users => users.concat(newUser));
+               setAnswer( JSON.parse(`{"message":"User ${newUser.username} has been added"}`));
+            });
     }
 
     function handleChangeNewUserName(e){
         setNewUser({'username': e.target.value});  
     }
 
-    function handleDeleteUser(e){
-        const del_id = e.target.id;
-        userService.deleteOneUser(del_id).then(setUsers(users => users.filter(item => item._id !== del_id)));
+    function handleDeleteUser(id){
+        userService.deleteOneUser(id).then(setUsers(users => users.filter(item => item._id !== id))).then(ans => setAnswer(ans));
+        
     }
     
-    function handleSaveUser(e) {
-        let id = e.target.id;
+    function handleSaveUser(id) {
+        let foundUser = users.find(item => item._id === id);
         userService.updateOneUser(
-            {'id' : id,
+            {'id' : foundUser._id,
            // 'username': document.getElementById(`username_${e.target.id}`).value,
-            'username': users.find(item => item._id === id).username, 
-            'password':users.find(item => item._id === id).password,
-            'firstName': users.find(item => item._id === id).firstName,
-            'lastName': users.find(item => item._id === id).lastName,
-            'email': users.find(item => item._id === id).email,
-            'isAdmin': users.find(item => item._id === id).isAdmin}
+            'username': foundUser.username, 
+            'password':foundUser.password,
+            'firstName': foundUser.firstName,
+            'lastName': foundUser.lastName,
+            'email': foundUser.email,
+            'isAdmin': foundUser.isAdmin}
             ).then(data => { 
                 setUsers(users => users.map((item) =>{
                     if (item._id === data._id) {
@@ -44,21 +51,23 @@ function AdminPage(){
                       } else {
                         return item;
                       }
-                } ))
+                } ));
+                setAnswer( JSON.parse(`{"message":"User ${data.username} has been changed"}`));
             });
     }
 
     function handleChangeField(e){
-        let field = e.target.id.split('_')[0];
-        let id = e.target.id.split('_')[1];
-        let value = e.target.value;
-        if (field ==='isAdmin') {
+        let { name, value, id } = e.target;
+        // let field = e.target.id.split('_')[0];
+        // let id = e.target.id.split('_')[1];
+        // let value = e.target.value;
+        if (name ==='isAdmin') {
             value =e.target.checked;
         }
         setUsers(users => users.map((item) =>{
             if (item._id === id) {
                 let temp = Object.assign({}, item);
-                temp[field] = value;
+                temp[name] = value;
                 return item = temp;    
               } else {
                 return item;
@@ -71,29 +80,36 @@ function AdminPage(){
 
     if (currentUser.isAdmin) {
         return (  
-            <div>
-                {currentUser.isAdmin?`Admin ${currentUser.username}`:`User ${currentUser.username}`}
-                {users.length &&
-                    <div>
+            <Grid container>
+                <Grid item>
+                    <Typography variant='h3' gutterBottom>
+                    Control Panel: {currentUser.username}
+                    </Typography>
+                </Grid>
+                    {users.length &&
+                    <Grid container>
                         {users.map((user, index) =>
 
-                            <div key={user._id} >
-                               {/* <User user={user} /> */}
-                                <input type="text" id={`username_${user._id}`} defaultValue={user.username} onChange={handleChangeField}/> 
-                                <input type="text" id={`password_${user._id}`} defaultValue={user.password} onChange={handleChangeField}/> 
-                                <input type="text" id={`firstName_${user._id}`} defaultValue={user.firstName} onChange={handleChangeField}/> 
-                                <input type="text" id={`lastName_${user._id}`} defaultValue={user.lastName} onChange={handleChangeField}/>           
-                                <input type="text" id={`email_${user._id}`} defaultValue={user.email} onChange={handleChangeField}/> 
-                                <input type="checkbox" id={`isAdmin_${user._id}`} defaultChecked={user.isAdmin} onClick={handleChangeField}/> 
-                                <input type="button" value="save"   id={user._id} onClick={handleSaveUser} />
-                                <input type="button" value="delete" id={user._id} onClick={handleDeleteUser} /><br /><br />
-                            </div>
+                            <Grid item key={user._id} >
+                                <TextField type="text_u"  label="Username"    id={user._id} name="username"   defaultValue={user.username}   onChange={handleChangeField}/> 
+                                <TextField type="text_p"  label="Password"    id={user._id} name="password"  defaultValue={user.password}  onChange={handleChangeField}/> 
+                                <TextField type="text_f"  label="First name"  id={user._id} name="firstName" defaultValue={user.firstName} onChange={handleChangeField}/> 
+                                <TextField type="text_l"  label="Last name" id={user._id} name="lastName" defaultValue={user.lastName}  onChange={handleChangeField}/>           
+                                <TextField type="text_e"  label="Email" id={user._id} name="email"     defaultValue={user.email}     onChange={handleChangeField}/> 
+                                <Checkbox type="checkbox" id={user._id} name="isAdmin" defaultChecked={user.isAdmin} onClick={handleChangeField}/> 
+                                <Button type="button" variant="contained" color="primary"   onClick={()=>handleSaveUser(user._id)} > Save </Button>
+                                <Button type="button" variant="contained" onClick={()=>handleDeleteUser(user._id)}> Delete </Button>
+                                <br /><br />
+                            </Grid>
                         )}
-                    </div>
+                    </Grid>
                 }
-                <input type="text" onChange={handleChangeNewUserName} />    
-                <input type="button" value="add" onClick={handleAddItem}  disabled={!newUser} />    
-            </div>)
+                <Grid item> 
+                    <TextField type="text_ a" onChange={handleChangeNewUserName} />    
+                    <Button variant="contained" color="primary" onClick={handleAddItem} disabled={!newUser} >Add</Button> <br /><br />
+                    {answer&& <Alert severity="success"><AlertTitle> {answer.message}</AlertTitle></Alert>}  
+                 </Grid>
+            </Grid>)
     }
     else 
         return (  
