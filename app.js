76ab10c9720803e6e10885a5508d339eef54a020
Nikita Kolchin <kolchin.nv@gmail.com>
2020-05-04 18:30:36 +0300

    0305_1

---
+++ app.js
@@ -0,0 +1,23 @@
+const MongoClient = require("mongodb").MongoClient;
+   
+const url = "mongodb+srv://nikitos:qwasdx=1@cloudcluster-yl09g.gcp.mongodb.net/test";
+const mongoClient = new MongoClient(url, { useNewUrlParser: true });
+console.log(mongoClient); 
+mongoClient.connect(function(err, client){
+    if (err) {
+        console.error('An error occurred connecting to MongoDB: ', err);
+    } else {  
+    const db = client.db("test");
+    const collection = db.collection("users");
+    let user = {username: "Tom"};
+    collection.insertOne(user, function(err, result){
+          
+        if(err){ 
+            return console.log(err);
+        }
+        console.log(result.ops);
+        client.close();
+    });
+
+}
+});
