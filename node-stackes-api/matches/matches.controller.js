87472e3ee91ec9261d0c3faf0fe9b74741239b78
Nikita Kolchin <kolchin.nv@gmail.com>
2020-05-08 17:31:40 +0300

    0805

--- node-stackes-api/matches/matches.controller.js
+++ node-stackes-api/matches/matches.controller.js
@@ -7,11 +7,11 @@ module.exports =  router;
 
 
 
-//get all users
+//get all matches
 router.get('/', async (req, res, next) => {
     try {
-        const users = await Match.find()
-        res.json(users)
+        const matches = await Match.find()
+        res.json(matches)
       } catch (err) {
         res.status(500).json({message: err.message})
       }
@@ -31,9 +31,6 @@ router.post('/', async (req, res, next) => {
       visability: req.body.visability
     });
     try {
-      // const newUser = await user.save();
-      // res.status(201).json({ newUser });
-
       res.status(201).json(await match.save());
     } catch (err) {
       res.status(400).json({ message: err.message });
@@ -46,38 +43,38 @@ router.post('/', async (req, res, next) => {
 });
 
 //middleware
-async function getUser (req, res, next) {
-    let user;
+async function getMatch (req, res, next) {
+    let match;
     try {
-      user = await Match.findById(req.params.id);
-      if (user == null) {
+      match = await Match.findById(req.params.id);
+      if (match == null) {
         return res.status(404).json({ message: "Cannot find User" });
       }
     } catch (err) {
       return res.status(500).json({ message: err.message });
     }
-    res.user = user;
+    res.match = match;
     next();
 }
 
 //get one user by id 
-router.get('/:id',getUser, (req, res) => {
-    res.json(res.user);
+router.get('/:id',getMatch, (req, res) => {
+    res.json(res.match);
 }); 
 
 //delete One user
-router.delete("/:id", getUser, async (req, res) => {
+router.delete("/:id", getMatch, async (req, res) => {
   if (req.user.isAdmin){
     try {
-      const deletedUserName = res.user.username; 
-      await res.user.deleteOne();
-      res.json({ message: `User ${deletedUserName} has been deleted` });
+      const deletedMatchNo = res.match.matchNo; 
+      await res.match.deleteOne();
+      res.json({ message: `Match ${deletedMatchNo} has been deleted` });
     } catch (err) {
       res.status(500).json({ message: err.message });
     }
   }
   else {
-    res.status(403).json({ message: "Need admin" });
+    res.status(403).json({ message: "Administrator rights required" });
   }  
 });
 
@@ -86,7 +83,7 @@ router.delete("/", async (req, res) => {
   if (req.user.isAdmin){
     try {
       await Match.deleteMany();
-      res.json({ message: "All users has been deleted" });
+      res.json({ message: "All matches has been deleted" });
     } catch (err) {
       res.status(500).json({ message: err.message });
     }
@@ -97,18 +94,20 @@ router.delete("/", async (req, res) => {
 });
 
 //update one user
-router.put("/:id", getUser, async (req, res) => {
+router.put("/:id", getMatch, async (req, res) => {
   if (req.user.isAdmin){
     try {
-        const updatedUser = {username: req.body.username, 
-                        password: req.body.password,
-                        firstName: req.body.firstName, 
-                        lastName: req.body.lastName,
-                        email: req.body.email,
-                        isAdmin: req.body.isAdmin
+        const updatedMatch = {matchNo: req.body.matchNo, 
+          home: req.body.home,
+          away: req.body.away, 
+          homeName: req.body.homeName,
+          awayName: req.body.awayName,
+          coefficient: req.body.coefficient,
+          enable: req.body.enable,
+          visability: req.body.visability
                        };  
-        const userArterUpdate = await Match.findOneAndUpdate({_id: res.user.id}, updatedUser, {new: true});
-        res.json(userArterUpdate);
+        const matchArterUpdate = await Match.findOneAndUpdate({_id: res.match.id}, updatedMatch, {new: true});
+        res.json(matchArterUpdate);
     } catch (err) {
         res.status(400).json({ message: err.message });
     }
@@ -117,21 +116,3 @@ router.put("/:id", getUser, async (req, res) => {
     res.status(403).json({ message: "Administrator rights required" });
   }  
 });
-
-
-//update and set stakes
-router.put("/:id/stakes", getUser, async (req, res) => {
-    try {
-        const updatedUser = {stakes: req.body.stakes};  
-        const userArterUpdate = await Match.findOneAndUpdate({_id: res.user.id}, updatedUser, {new: true});
-        res.json(userArterUpdate);
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
-});
-
-//get one user by id 
-router.get('/:id/stakes',getUser, (req, res) => {
-  res.json(res.user.stakes);
-}); 
-
