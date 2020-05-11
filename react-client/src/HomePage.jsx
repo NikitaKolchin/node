66a11b26bfefe6c5db95eaea3afefd364c9bda54
Nikita Kolchin <kolchin.nv@gmail.com>
2020-05-11 20:49:30 +0300

    1105_2

--- react-client/src/HomePage.jsx
+++ react-client/src/HomePage.jsx
@@ -4,30 +4,34 @@ import MaterialTable from 'material-table';
 import { backendService } from './backend.service';
 
 function HomePage() {
-    const info =
-     [{ matchNo: 1,  home: null, away: null, homeName: 'Russia', awayName: 'Scotland', coefficient: 1, enable: true, visability: true}, 
-      { matchNo: 2,  home: null, away: null, homeName: 'England', awayName: 'Spain', coefficient: 1, enable: true, visability: true} ];
+    // const info =
+    //  [{ matchNo: 1,  home: null, away: null, homeName: 'Russia', awayName: 'Scotland', coefficient: 1, enable: true, visability: true}, 
+    //   { matchNo: 2,  home: null, away: null, homeName: 'England', awayName: 'Spain', coefficient: 1, enable: true, visability: true} ];
     let [currentUser, setCurrentUser] = useState({});
     let [stakes, setStakes] = useState({loading: true});
+    let [matches, setMatches] = useState({loading: true});
 
     useEffect(()=>{
         setCurrentUser(JSON.parse(localStorage.getItem('user')));
         (async () => {
             setStakes(await backendService.getStakesByUserId(JSON.parse(localStorage.getItem('user'))._id));
         })();  
-    },[]);
+        (async () => {
+            setMatches(await backendService.getAll('matches'));
+        })(); 
+    },[]); //отсортировать массивы
 
     return( 
             <div>
                 <h1>Привет, {currentUser.firstName}!</h1>
                 <h3>Сделать ставки можно здесь:</h3>
                 {stakes.loading && <em>Loading stakes...</em>}
-                {stakes.length &&
+                {stakes.length && matches.length &&
                     <ul>
                         {stakes.map((stake, index) =>
                             <li key={stake._id}> 
-                                {info[index].homeName+ ' '+stake.home +' ' + info[index].awayName+' ' + stake.away}
-                            </li>     
+                                {matches[index].homeName+ ' '+stake.home +' ' + matches[index].awayName+' ' + stake.away}
+                            </li>    //или тут отсортировать массивы 
                         )}
                     </ul>
                 }
