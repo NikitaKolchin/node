8398931bbda63ba8f7e8155e091affb6e7c83e10
Nikita Kolchin <kolchin.nv@gmail.com>
2020-06-05 21:54:02 +0300

    0506_1

---
+++ src/server/build/precache-manifest.ceef44147269e4c57d1c463a06503569.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56651334f3285bc245c0fdf998bd588b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccbf990ddcf17e1d7bfb",
+    "url": "/static/js/2.f1e743f7.chunk.js"
+  },
+  {
+    "revision": "031461381450067598a8d0b4a7cca573",
+    "url": "/static/js/2.f1e743f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a335f0cb82827d6e02bb",
+    "url": "/static/js/main.bd225cd9.chunk.js"
+  },
+  {
+    "revision": "48e36f86f65c8ea00987",
+    "url": "/static/js/runtime-main.1090b06e.js"
+  }
+]);
