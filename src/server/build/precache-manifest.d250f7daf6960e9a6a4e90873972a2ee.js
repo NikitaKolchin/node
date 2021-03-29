230d2f20bc576f7daa608bf4916fc0247f1d22e0
Nikita Kolchin <kolchin.nv@gmail.com>
2021-03-29 18:21:13 +0300

    2903_1

---
+++ src/server/build/precache-manifest.d250f7daf6960e9a6a4e90873972a2ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61c81398d8b13846bb1f11b08c777c3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44ce7cda7a1e17935441",
+    "url": "/static/js/2.727c94ff.chunk.js"
+  },
+  {
+    "revision": "031461381450067598a8d0b4a7cca573",
+    "url": "/static/js/2.727c94ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff01c80b5ddd29617ecc",
+    "url": "/static/js/main.970575ef.chunk.js"
+  },
+  {
+    "revision": "48e36f86f65c8ea00987",
+    "url": "/static/js/runtime-main.1090b06e.js"
+  }
+]);
