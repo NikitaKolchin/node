18ac3939664d578c05a4e49c2d5987333f2368e5
Колчин Никита Вячеславович <kolchin.nv@gmail.com>
2021-06-06 14:50:18 +0300

    210606

---
+++ src/server/build/precache-manifest.3fc1f1916b4aa8ad0540cffa4ae12d61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01f591a69580fb28bb1cc55bb61edebf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfe6d7661bbc7228b0ff",
+    "url": "/static/js/2.dc11d4a1.chunk.js"
+  },
+  {
+    "revision": "031461381450067598a8d0b4a7cca573",
+    "url": "/static/js/2.dc11d4a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d299b6c6305e61112347",
+    "url": "/static/js/main.5fa9b4f6.chunk.js"
+  },
+  {
+    "revision": "48e36f86f65c8ea00987",
+    "url": "/static/js/runtime-main.1090b06e.js"
+  }
+]);
